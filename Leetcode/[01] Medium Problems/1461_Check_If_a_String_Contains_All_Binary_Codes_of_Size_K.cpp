112bd58b3dc6c038a012a233e7673a99207ec912
RUT KOTICHA <153355434+RutKoticha@users.noreply.github.com>
2026-03-22 11:40:57 +0530

    feat: implement solutions for binary tree sum and integer sorting problems

---
+++ Leetcode/[01] Medium Problems/1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file 1461_Check_If_a_String_Contains_All_Binary_Codes_of_Size_K.cpp
+ * @author Rut Koticha
+ * @date 2026-02-23
+ */
+
+#include <string>
+
+class Solution {
+public:
+    bool hasAllCodes(std::string s, int k) {
+        
+    }
+};
--- Leetcode/[01] Medium Problems/7_Reverse_Integer.cpp
+++ Leetcode/[01] Medium Problems/7_Reverse_Integer.cpp
@@ -10,20 +10,42 @@
 
 class Solution {
 public:
-  int32_t reverse(int32_t number)
-  {
-    int32_t reversed = 0, remainder = 0;
+	int reverse(int number)
+	{
+		const int MAX_REVERSED = INT_MAX / 10;
+		const int MIN_REVERSED = INT_MIN / 10;
+		const int MAX_REMAINDER = INT_MAX % 10;
+		const int MIN_REMAINDER = INT_MIN % 10;
 
-    while (number)
-    {
-      if (std::abs(reversed) >= (INT32_MAX / 10))
-        return 0;
+		int reversed = 0;
 
-      remainder = number % 10;
-      reversed = (10 * reversed) + remainder;
-      number /= 10;
-    }
+		while (number)
+		{
+			int remainder = number % 10;
+			number /= 10;
 
-    return reversed;
-  }
+			bool isReversedAtMin = reversed == MIN_REVERSED;
+			bool isReversedAtMax = reversed == MAX_REVERSED;
+			bool isReversedGreater = reversed > MAX_REVERSED;
+			bool isReversedSmaller = reversed < MIN_REVERSED;
+			bool isRemainderGreater = remainder >= MAX_REMAINDER;
+			bool isRemainderSmaller = remainder <= MIN_REMAINDER;
+
+			// If adding 0 would make them out of range
+			if (isReversedGreater || isReversedSmaller)
+				return 0;
+
+			// If adding the remainder make it go positively out
+			if (isReversedAtMax && isRemainderGreater)
+				return 0;
+
+			// If adding the remainder make it go negatively out
+			if (isReversedAtMin && isRemainderSmaller)
+				return 0;
+
+			reversed = (reversed * 10) + remainder;
+		}
+
+		return reversed;
+	}
 };
